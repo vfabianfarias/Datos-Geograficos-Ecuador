92498236cb6d9d58863318386acfaa50e39fbda9
vfabianfarias <vfabianfarias@gmail.com>
2016-08-07 18:39:13 -0500

    Update provincias.sql

--- provincias.sql
+++ provincias.sql
@@ -1,3 +1,15 @@
+SET NAMES 'utf8';
+
+CREATE TABLE tbl_provincia(
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  provincia TEXT NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 1
+CHARACTER SET utf8
+COLLATE utf8_spanish_ci;
+
 INSERT INTO tbl_provincia VALUES 
   (1, 'Azuay'),
   (2, 'Bolívar'),
